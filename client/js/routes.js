b7a97d78853f26e4858198c56c1e6872cee262f8
Sarah Svedenborg <sarah.svedenborg@gmail.com>
2016-03-08 13:49:32 +0100

    created html for network page

--- client/js/routes.js
+++ client/js/routes.js
@@ -29,6 +29,11 @@ function config($stateProvider, $urlRouterProvider) {
       url: '/LoggedIn',
       templateUrl: 'client/templates/LoggedInScreen.html'
     })
+    .state('network',{
+      url: '/network',
+      templateUrl: 'client/templates/network.html',
+      controller: 'networkController'
+    })
 
   $urlRouterProvider.otherwise('/');
 }
