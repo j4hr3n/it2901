05f5481af9c6fdf6280b5b80348d4a44abeaadae
Emil S. Andersen <Emil-andersen@live.no>
2016-04-01 14:18:05 +0200

    design and padding

--- client/js/controllers/profileController.js
+++ client/js/controllers/profileController.js
@@ -4,7 +4,7 @@ angular
 
 function profileCtrl($scope, $reactive) {
   $reactive(this).attach($scope);
-  
+
   this.helpers({
     user: () => {
       return Meteor.user() || Meteor.users.findOne({});
@@ -28,14 +28,55 @@ function profileCtrl($scope, $reactive) {
 
     Users.update(
       { _id: this.user._id },
-      { 
-        $set: { 
+      {
+        $set: {
         name: this.user.name,
         nameFirst: this.user.nameFirst,
         nameLast: this.user.nameLast,
-        friends: this.user.friends } 
+        friends: this.user.friends }
       }
     )
   };
-  
-}
+
+    var value = 1;
+    var pos = 0;
+    var progressHidden = false;
+    var progressReset = false;
+    var delay = 40;
+    var progressEl = $('progress');
+
+    // set interval
+    var timer = setInterval(progress, 1000);
+
+    function progress() {
+
+      // run counter
+      value++;
+      if (value < 10) {
+        progressEl.val(value);
+        pos = 1 - (value / 10);
+      } else if (value < (delay + 10)) {
+        progressEl.val(10);
+        pos = 0;
+      } else {
+        value = 1;
+      }
+
+      // update background
+      progressEl.css('background-position', '0 ' + pos + 'em');
+
+      // show/hide progress
+      if (!progressHidden && value >= 10) {
+        // progressEl.addClass("hidden");
+        // progressHidden = true;
+        progressReset = true
+
+      } else if (progressHidden && value < 10 || progressReset) {
+        progressEl.val(0);
+        progressEl.removeClass("hidden");
+        progressHidden = false;
+        progressReset = false;
+      }
+
+    }
+  }
