865ebc660724fd6f9a5bf9976269822bd9202b75
Emil S. Andersen <Emil-andersen@live.no>
2016-04-26 14:26:44 +0200

    asdf search

--- client/js/controllers/indexController.js
+++ client/js/controllers/indexController.js
@@ -2,7 +2,9 @@ angular
     .module('it2901')
     .controller('indexCtrl', indexCtrl);
 
-function indexCtrl($scope, $reactive, $location) {
+
+
+function indexCtrl($scope, $reactive, $location, $filter) {
   $reactive(this).attach($scope);
 
   var _selected;
@@ -32,6 +34,10 @@ function indexCtrl($scope, $reactive, $location) {
     }
   });
 
+  // $scope.globalSearch = function(keyword){
+  //   return $filter('filter')(keyword, {type: user.username} && {type: user.profile.nameFirst} && {type: user.profile.nameLast})
+  //
+  // }
 
 //Test
   // function filterByName(users.profile, typeValue){
